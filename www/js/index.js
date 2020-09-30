bbb4824bd7fcb80a09504e0c9f211e3c020685b1
takahiro ootani <taka.oot@gmail.com>
2020-09-30 11:49:01 +0900

    位置情報の定期的な取得

--- www/js/index.js
+++ www/js/index.js
@@ -1,17 +1,17 @@
 // 各ページの初期設定
-document.addEventListener('init', function (event) {
+document.addEventListener("init", function (event) {
   // 表示対象のページを取得
   var page = event.target;
 
-  if (page.matches('#list-page')) {
+  if (page.matches("#list-page")) {
     // タスクを取得
     getTaskslist();
   }
-  if (page.matches('#list-page-completed')) {
+  if (page.matches("#list-page-completed")) {
     // 完了済みタスクを取得
     getTaskslist(true);
-  }
-else { //暫定的。
+  } else {
+    //暫定的。
     // 完了済みタスクを取得
     getTaskslistEdit();
   }
@@ -25,9 +25,30 @@ function onDeviceReady() {
   // 認証済みかどうか判定
   if (window.localStorage.getItem("refresh_token")) {
     getAccessTokenByRefreshToken();
+    window.cordova.plugins.backgroundMode.setEnabled(true); //バックグラウンド起動
+    var watchID = GPSLocation.watchPosition(onSuccess, onError, {
+      timeout: 600000,
+    });
+    cordova.plugins.notification.local.schedule({
+      title: "TODOリストアプリの通知",
+      text: "タスクに設定した位置に近くなると通知します",
+      foreground: true,
+    });
   } else {
     // ログイン画面を表示
-    document.querySelector('#appNavigator').pushPage('login.html', { animation: "fade" });
+    document
+      .querySelector("#appNavigator")
+      .pushPage("login.html", { animation: "fade" });
   }
 }
 
+function onSuccess(position) {
+  var element = document.getElementById("geolocation");
+  console.log(position.coords.latitude + "," + position.coords.longitude);
+}
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+  alert("code: " + error.code + "\n" + "message: " + error.message + "\n");
+}
